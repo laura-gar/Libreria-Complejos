e0d445cb321b45a81b296a4e4fb3a4a8a0aaa6a5
laura-gar <60012037+laura-gar@users.noreply.github.com>
2020-01-30 15:04:18 -0500

    Update ComplexLib.py

--- ComplexLib.py
+++ ComplexLib.py
@@ -148,4 +148,14 @@ def matrizAdjunta(m1):
     m = matrizConjugada(transpuesta(m1))
     return m 
 
+def multiplicacionMatrices(m1, m2):
+    matriz = [[0 for j in range(len(m2[0]))]for i in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                print(m1[i][k] , m2[k][j])
+                
+                matriz[i][j] += m1[i][k] * m2[k][j]
+    return matriz
+
 
