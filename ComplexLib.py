d2d583a090be83508aab6770b1c62a72129f0276
laura-gar <60012037+laura-gar@users.noreply.github.com>
2020-02-04 16:56:04 -0500

    Update ComplexLib.py

--- ComplexLib.py
+++ ComplexLib.py
@@ -4,6 +4,7 @@
 
 from sys import stdin
 import math
+math.tan
 
 def suma(a,b):
     '''Función que suma dos números complejos
@@ -19,12 +20,15 @@ def resta(a,b):
     d = a[1] - b[1]
     return (c,d)
 
-def multiplicacion(a,b):
+def multCom(a,b):
     '''Función que multiplica dos números complejos
        (tupla de float,tupla de float) -> (tupla de float)'''
     c = a[0]*b[0] - a[1]*b[1]
     d = a[0]*b[1] + a[1]*b[0]
     return (c,d)
+
+def multRe (a,b):
+    return a*b
 
 def division(a,b):
     '''Función que divide dos números complejos
@@ -50,10 +54,11 @@ def returnPhase(a):
        (tupla de float) -> (float)'''
     ang = None
     if (a[0]<0 and a[1]>0) or (a[0]<0 and a[1]<0):
-        ang = (math.atan(a[1]/a[0])+(math.pi))
+        ang = round((math.atan(a[1]/a[0])+(math.pi)),2)
+    elif a[0]>0 and a[1]<0:
+        ang = round((math.atan(a[1]/a[0])+2*(math.pi)),2)
     else:
-        ang = math.atan(a[1]/a[0])
-    ang = ang * 180/math.pi
+        ang = round(math.atan(a[1]/a[0]),2)
     return ang
 
 def cartToPol(a):
@@ -86,20 +91,19 @@ def productoEscalar(c,v1):
        (tupla de float, lista)->(lista)'''
     vector = []
     for i in range(len(v1)):
-        vector += [multiplicacion(c,v1[i])]
+        vector += [multCom(c,v1[i])]
     return vector
 
 def sumaMatrices(m1,m2):
     '''Función que realiza la suma de dos matrices de números complejos
        (matriz,matriz)->(matriz)'''
     if len(m1)==len(m2) and len(m1[0])==len(m2[1]):
-        matriz = []
+        matriz = [[0 for j in range(len(m1[0]))]for i in range(len(m1))]
         for i in range(len(m1)):
-            fila = []
             for j in range(len(m1[0])):
-                fila += [suma(m1[i][j],m2[i][j])]
-            matriz += [fila]
-            return matriz
+                matriz[i][j]= suma(m1[i][j],m2[i][j])
+            
+        return matriz
     else:
         print('Error')
 
@@ -148,15 +152,53 @@ def matrizAdjunta(m1):
     m = matrizConjugada(transpuesta(m1))
     return m 
 
-def multiplicacionMatrices(m1, m2):
-    matriz = [[0 for j in range(len(m2[0]))]for i in range(len(m1))]
+def multMatCom(m1, m2):
+    matriz = [[(0,0) for j in range(len(m2[0]))]for i in range(len(m1))]
     for i in range(len(m1)):
         for j in range(len(m2[0])):
             for k in range(len(m2)):
-                print(m1[i][k] , m2[k][j])
-                
-                matriz[i][j] += m1[i][k] * m2[k][j]
+                matriz[i][j] = suma(matriz[i][j],multCom(m1[i][k],m2[k][j]))
     return matriz
 
-def hermitain(m1):
+##def multMatRea(m1, m2):
+##    matriz = [[0 for j in range(len(m2[0]))]for i in range(len(m1))]
+##    for i in range(len(m1)):
+##        for j in range(len(m2[0])):
+##            for k in range(len(m2)):
+##                matriz[i][j] += m1[i][k]*m2[k][j]
+##    return matriz
+
+##def innerInt(v1,v2):
+##    res = multMatRea(transpuesta(v1),v2)
+##    return res[0][0]
+
+def innerProduct(v1,v2):
+    print(matrizAdjunta(v1))
+    res = multMatCom(matrizAdjunta(v1),v2)
+    return res[0][0]
+
+
+def matrizHermitain(m1):
     return (m1 == matrizAdjunta(m1))
+
+def matrizUnitary(m1):
+    matId = [[(0,0) for j in range(len(m1))]for i in range(len(m1))]
+    for i in range(len(m1)):
+        matId[i][i] = (1,0)
+    print(matId)
+    a, b, c = multiplicacionMatrices(m1,matrizAdjunta(m1)),  multiplicacionMatrices(matrizAdjunta(m1),m1), matId
+    print(a)
+    if a == b == c:
+        return True
+    else:
+        return False
+
+def prodTensor(v1,v2):
+    tensor = [(0,0) for i in range(len(v1)*len(v2))]
+    pos = 0
+    for i in range(len(v1)):
+        for j in range(len(v2)):
+            tensor[pos] = multCom(v1[i],v2[j])
+            pos += 1
+    return tensor
+
